bf56293a3d100dcedf788ed1c8c3899291de6200
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2024-08-07 13:31:33 +0200

    Initial commit

---
+++ dnsconfig.js
@@ -0,0 +1,15 @@
+DEFAULTS(
+    DefaultTTL(86400),
+    NAMESERVER_TTL(86400)
+);
+
+var REG_MONITOR = NewRegistrar("dohdefault");
+var DNS_CLOUDNS = NewDnsProvider("cloudns");
+
+var REG_GANDI = NewRegistrar("gandi");
+var DNS_GANDI = NewDnsProvider("gandi");
+
+var REG_GANDI_GR = NewRegistrar("gandigr");
+var DNS_GANDI_GR = NewDnsProvider("gandigr");
+
+require_glob('./domains/');
