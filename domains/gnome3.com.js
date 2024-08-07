4039970983d34d2eea166ce400cef992ccb40405
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2024-08-07 13:31:45 +0200

    Import gnome3.com

---
+++ domains/gnome3.com.js
@@ -0,0 +1,8 @@
+D("gnome3.com", REG_GANDI,
+    DnsProvider(DNS_GANDI),
+    MX("@", 10, "smtp.gnome.org."),
+    A("@", "8.43.85.3"),
+    A("@", "8.43.85.4"),
+    A("@", "8.43.85.5"),
+    CNAME("www", "router-default.apps.openshift4.gnome.org."),
+    END);
