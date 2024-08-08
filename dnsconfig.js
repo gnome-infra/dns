e2473e032c1da8d2d34982cefc89fdb334f8f2a2
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2024-08-08 09:26:57 +0200

    Revert "Drop gnome.gr"
    This reverts commit bf7d3c2a5f0909a1a114177b20a87b24ab51407c.

--- dnsconfig.js
+++ dnsconfig.js
@@ -8,4 +8,7 @@ var REG_MONITOR = NewRegistrar("dohdefault");
 var REG_GANDI = NewRegistrar("gandi");
 var DNS_GANDI = NewDnsProvider("gandi");
 
+var REG_GANDI_GR = NewRegistrar("gandigr");
+var DNS_GANDI_GR = NewDnsProvider("gandigr");
+
 require_glob('./domains/');
