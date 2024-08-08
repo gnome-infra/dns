cdd242b648954f6f2b9d079667d7cc7c938a858b
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2024-08-08 09:44:21 +0200

    Import gnomehispano domains

--- dnsconfig.js
+++ dnsconfig.js
@@ -8,4 +8,7 @@ var REG_MONITOR = NewRegistrar("dohdefault");
 var REG_GANDI = NewRegistrar("gandi");
 var DNS_GANDI = NewDnsProvider("gandi");
 
+var REG_GANDI_ES = NewRegistrar("gandi_es");
+var DNS_GANDI_ES = NewDnsProvider("gandi_es");
+
 require_glob('./domains/');
