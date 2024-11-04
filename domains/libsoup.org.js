1c01c2e2ad601a161b27bbffc5d06d211507e516
Andrea Veri <averi@redhat.com>
2024-11-04 16:32:40 -0500

    libsoup.org will not be renewed

--- domains/libsoup.org.js
+++
@@ -1,8 +0,0 @@
-D("libsoup.org", REG_GANDI,
-    DnsProvider(DNS_GANDI),
-    A("@", "8.43.85.3"),
-    A("@", "8.43.85.4"),
-    A("@", "8.43.85.5"),
-    TXT("@", "google-site-verification=Mgpz9pg08HGTf7Npx1NQ7bZ3IaG1v0ww9QiS_HB6u4o"),
-    CNAME("www", "router-default.apps.openshift4.gnome.org."),
-    END);
