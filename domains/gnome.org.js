7943435f56f395ce6abfddd02c619ff153f0a756
Andrea Veri <averi@redhat.com>
2024-11-11 13:58:46 -0500

    Missing trailing dot

--- domains/gnome.org.js
+++ domains/gnome.org.js
@@ -264,7 +264,7 @@ D("gnome.org", REG_GANDI,
     CNAME("ostree", "1029332277.rsc.cdn77.org."),
 
     // CDN
-    CNAME("download", "1673558209.rsc.cdn77.org"),
+    CNAME("download", "1673558209.rsc.cdn77.org."),
     CNAME("static", "1009550802.rsc.cdn77.org."),
     CNAME("www-old", "1009550802.rsc.cdn77.org."),
 
