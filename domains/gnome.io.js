da9b489516d7768c2ca3d56a928131d4cb57bcc4
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2025-09-11 19:58:41 +0200

    Add wiki.gnome.io

--- domains/gnome.io.js
+++ domains/gnome.io.js
@@ -4,4 +4,5 @@ D("gnome.io", REG_GANDI,
     MX("@", 10, "smtp.gnome.org."),
     ALIAS("@", "ingress.openshift.gnome.org."),
     CNAME("www", "ingress.openshift.gnome.org."),
+    CNAME("wiki", "ingress.openshift.gnome.org."),
     END);
