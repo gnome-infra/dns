8ee3aea8bd4c409c31ece4bb39d5ff0e6a58c1d5
Bart Piotrowski <bpiotrowski@gnome.org>
2026-01-27 09:55:42 +0100

    Add mixpost.gnome.io

--- domains/gnome.io.js
+++ domains/gnome.io.js
@@ -4,5 +4,7 @@ D("gnome.io", REG_GANDI,
     MX("@", 10, "smtp.gnome.org."),
     ALIAS("@", "ingress.openshift.gnome.org."),
     CNAME("www", "ingress.openshift.gnome.org."),
+
+    CNAME("mixpost", "ingress.openshift.gnome.org."),
     CNAME("wiki", "ingress.openshift.gnome.org."),
     END);
